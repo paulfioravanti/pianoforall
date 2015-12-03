1af0d49c2851ac8c5aa816d8c6ac93bc61563d23
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-12-04 08:42:57 +1100

    Add lesson 6

---
+++ lesson_6/let_it_be.rb
@@ -0,0 +1,42 @@
+# Lesson 6 - Gospel Song Chord Progression
+# Let It Be
+use_synth :piano
+
+play :C3
+play chord(:C4)
+sleep 0.5
+play chord(:C4)
+sleep 0.5
+play :G3
+play chord(:G4)
+sleep 0.5
+play chord(:G4)
+sleep 0.5
+play :A3
+play chord(:A4, :minor)
+sleep 0.5
+play chord(:A4, :minor)
+sleep 0.5
+play :F3
+play chord(:F4)
+sleep 0.5
+play chord(:F4)
+sleep 0.5
+play :C3
+play chord(:C4)
+sleep 0.5
+play chord(:C4)
+sleep 0.5
+play :G3
+play chord(:G4)
+sleep 0.5
+play chord(:G4)
+sleep 0.5
+play :F3
+play chord(:F4)
+sleep 0.5
+play chord(:E4, :minor)
+sleep 0.25
+play chord(:D4, :minor)
+sleep 0.25
+play chord(:C4)
---
+++ lesson_6/straight_beat_gospel_style.rb
@@ -0,0 +1,23 @@
+# Lesson 6 - Straight Beat Gospel Style
+use_synth :piano
+
+# Chord Progression
+play :C3
+play chord(:C4)
+sleep 0.5
+play chord(:C4)
+sleep 0.5
+play :A2
+play chord(:A3, :minor)
+sleep 0.5
+play chord(:A3, :minor)
+sleep 0.5
+play :F2
+play chord(:F3)
+sleep 0.5
+play chord(:F3)
+sleep 0.5
+play :G2
+play chord(:G3)
+sleep 0.5
+play chord(:G3)
