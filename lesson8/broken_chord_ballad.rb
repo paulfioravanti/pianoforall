c196aaea6a1480de63a80a1aa0fe194b1409e27d
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-12-06 12:15:58 +1100

    Add Broken Chord Ballad

---
+++ lesson8/broken_chord_ballad.rb
@@ -0,0 +1,212 @@
+# Lesson 8 - The Broken Chord Ballad
+use_synth :piano
+
+play :C2
+sleep 0.5
+
+play :C3
+sleep 0.25
+play :E3
+sleep 0.25
+play :G3
+sleep 0.25
+
+play :C4
+sleep 0.25
+play :E4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :C5
+sleep 0.25
+play :E5
+sleep 0.25
+play :G5
+sleep 0.25
+
+sleep 0.25 # ----
+
+play :F2
+sleep 0.5
+
+play :F3
+sleep 0.25
+play :A3
+sleep 0.25
+play :C4
+sleep 0.25
+
+play :F4
+sleep 0.25
+play :A4
+sleep 0.25
+play :C5
+sleep 0.25
+
+play :F5
+sleep 0.25
+play :A5
+sleep 0.25
+play :C6
+sleep 0.25
+
+sleep 0.25 # ----
+
+play :B2
+sleep 0.5
+
+play :B3
+sleep 0.25
+play :D4
+sleep 0.25
+play :F4
+sleep 0.25
+
+play :B4
+sleep 0.25
+play :D5
+sleep 0.25
+play :F5
+sleep 0.25
+
+play :B5
+sleep 0.25
+play :D6
+sleep 0.25
+play :F6
+sleep 0.25
+
+sleep 0.25 # ----
+
+play :E2
+sleep 0.5
+
+play :E3
+sleep 0.25
+play :G3
+sleep 0.25
+play :B3
+sleep 0.25
+
+play :E4
+sleep 0.25
+play :G4
+sleep 0.25
+play :B4
+sleep 0.25
+
+play :E5
+sleep 0.25
+play :G5
+sleep 0.25
+play :B5
+sleep 0.25
+
+sleep 0.25 # ----
+
+play :A2
+sleep 0.5
+
+play :A3
+sleep 0.25
+play :C4
+sleep 0.25
+play :E4
+sleep 0.25
+
+play :A4
+sleep 0.25
+play :C5
+sleep 0.25
+play :E5
+sleep 0.25
+
+play :A5
+sleep 0.25
+play :C6
+sleep 0.25
+play :E6
+sleep 0.25
+
+sleep 0.25 # ----
+
+play :D2
+sleep 0.5
+
+play :D3
+sleep 0.25
+play :F3
+sleep 0.25
+play :A3
+sleep 0.25
+
+play :D4
+sleep 0.25
+play :F4
+sleep 0.25
+play :A4
+sleep 0.25
+
+play :D5
+sleep 0.25
+play :F5
+sleep 0.25
+play :A5
+sleep 0.25
+
+sleep 0.25 # ----
+
+play :G2
+sleep 0.5
+
+play :G3
+sleep 0.25
+play :B3
+sleep 0.25
+play :D4
+sleep 0.25
+
+play :G4
+sleep 0.25
+play :B4
+sleep 0.25
+play :D5
+sleep 0.25
+
+play :G5
+sleep 0.25
+play :B5
+sleep 0.25
+play :D6
+sleep 0.25
+
+sleep 0.25 # ----
+
+play :C2
+sleep 0.5
+
+play :C3
+sleep 0.25
+play :E3
+sleep 0.25
+play :G3
+sleep 0.25
+
+play :C4
+sleep 0.25
+play :E4
+sleep 0.25
+play :G4
+sleep 0.25
+
+play :C5
+sleep 0.25
+play :E5
+sleep 0.25
+play :G5
+sleep 0.25
+
+sleep 0.75
+
+play :C6
