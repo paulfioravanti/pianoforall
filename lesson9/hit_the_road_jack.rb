130c1a715e78824b005bb8a56f4f633bf213e200
Paul Fioravanti <paul.fioravanti@gmail.com>
2015-12-27 22:49:31 +1100

    Add first song from Lesson 9

---
+++ lesson9/hit_the_road_jack.rb
@@ -0,0 +1,22 @@
+# Lesson 9 - More Chords: D, E, A and B Flat
+# Rhythm - Left Note/Right Chord (Hit the Road, Jack)
+use_synth :piano
+
+play :D3
+sleep 0.5
+play chord(:D4, :minor)
+sleep 0.5
+
+play :C3
+sleep 0.5
+play chord(:C4)
+sleep 0.5
+
+play :Bb2
+sleep 0.5
+play chord(:Bb3)
+sleep 0.25
+
+play :A2
+sleep 0.75
+play chord(:A3)
