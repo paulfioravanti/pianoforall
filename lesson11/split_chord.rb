2bb2d04a96c2b06961075f88a66542dd2db4b19a
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-02 22:37:24 +1100

    Allow split chord to accept a number of split chord reps

--- lesson11/split_chord.rb
+++ lesson11/split_chord.rb
@@ -1,12 +1,12 @@
 # Lesson 11 - Rhythm 4 - 'Split Chord'
 # 'Split Chord'
 
-def split_chord_c4(chord_name: :major, sleep_time: 0.5)
+def split_chord_c4(chord_name: :major, sleep_time: 0.5, split_reps: 3)
   play :C3, sustain: 3.5
   play chord(:C4, chord_name)
   sleep sleep_time
 
-  3.times do
+  split_reps.times do
     play :C4
     sleep sleep_time
 
@@ -18,12 +18,12 @@ def split_chord_c4(chord_name: :major, sleep_time: 0.5)
   sleep sleep_time
 end
 
-def split_chord_d4(chord_name: :major, sleep_time: 0.5)
+def split_chord_d4(chord_name: :major, sleep_time: 0.5, split_reps: 3)
   play :D3, sustain: 3.5
   play chord(:D4, chord_name)
   sleep sleep_time
 
-  3.times do
+  split_reps.times do
     play :D4
     sleep sleep_time
 
@@ -35,12 +35,12 @@ def split_chord_d4(chord_name: :major, sleep_time: 0.5)
   sleep sleep_time
 end
 
-def split_chord_e4(chord_name: :major, sleep_time: 0.5)
+def split_chord_e4(chord_name: :major, sleep_time: 0.5, split_reps: 3)
   play :E3, sustain: 3.5
   play chord(:E4, chord_name)
   sleep sleep_time
 
-  3.times do
+  split_reps.times do
     play :E4
     sleep sleep_time
 
@@ -52,12 +52,12 @@ def split_chord_e4(chord_name: :major, sleep_time: 0.5)
   sleep sleep_time
 end
 
-def split_chord_f4(chord_name: :major, sleep_time: 0.5)
+def split_chord_f4(chord_name: :major, sleep_time: 0.5, split_reps: 3)
   play :F3, sustain: 3.5
   play chord(:F4, chord_name)
   sleep sleep_time
 
-  3.times do
+  split_reps.times do
     play :F4
     sleep sleep_time
 
@@ -69,12 +69,12 @@ def split_chord_f4(chord_name: :major, sleep_time: 0.5)
   sleep sleep_time
 end
 
-def split_chord_g4(chord_name: :major, sleep_time: 0.5)
+def split_chord_g4(chord_name: :major, sleep_time: 0.5, split_reps: 3)
   play :G3, sustain: 3.5
   play chord(:G4, chord_name)
   sleep sleep_time
 
-  3.times do
+  split_reps.times do
     play :G4
     sleep sleep_time
 
@@ -86,12 +86,12 @@ def split_chord_g4(chord_name: :major, sleep_time: 0.5)
   sleep sleep_time
 end
 
-def split_chord_a4(chord_name: :major, sleep_time: 0.5)
+def split_chord_a4(chord_name: :major, sleep_time: 0.5, split_reps: 3)
   play :A3, sustain: 3.5
   play chord(:A4, chord_name)
   sleep sleep_time
 
-  3.times do
+  split_reps.times do
     play :A4
     sleep sleep_time
 
