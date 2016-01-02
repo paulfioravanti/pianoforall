02607c08579461be8c8717bebc872dba4dc5309c
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-02 22:38:12 +1100

    Add Something and Streets of London

---
+++ lesson11/streets_of_london.rb
@@ -0,0 +1,13 @@
+# Lesson 11 - Rhythm 4 - 'Split Chord'
+# Streets of London - Ralph McTell
+eval(
+  IO.read("#{Dir.home}/ruby/pianoforall/lesson11/split_chord.rb"),
+  binding
+)
+
+use_synth :piano
+
+split_chord_c4(split_reps: 1)
+split_chord_g4(split_reps: 1)
+split_chord_a4(chord_name: :minor, split_reps: 1)
+split_chord_e4(chord_name: :minor, split_reps: 1)
