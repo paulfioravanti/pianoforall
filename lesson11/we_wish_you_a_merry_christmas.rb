8d1e4992ff734e3ea84fa642b952e32f3118e959
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-02 22:38:00 +1100

    Get We wish you a Merry Christmas to use split_reps rather than inline the method

--- lesson11/we_wish_you_a_merry_christmas.rb
+++ lesson11/we_wish_you_a_merry_christmas.rb
@@ -6,40 +6,6 @@ eval(
 )
 
 use_synth :piano
-SLEEP_TIME = 0.3
-
-define :fast_f_fast_g do
-  play :F3, sustain: 3.5
-  play chord(:F4)
-  sleep SLEEP_TIME
-
-  1.times do
-    play :F4
-    sleep SLEEP_TIME
-
-    play [:A4, :C5]
-    sleep SLEEP_TIME
-  end
-
-  play :F4
-  sleep SLEEP_TIME
-
-  play :G3, sustain: 3.5
-  play chord(:G4)
-  sleep SLEEP_TIME
-
-  1.times do
-    play :G4
-    sleep SLEEP_TIME
-
-    play [:B4, :D5]
-    sleep SLEEP_TIME
-  end
-
-  play :G4
-  sleep SLEEP_TIME
-
-end
 
 split_chord_c4(sleep_time: SLEEP_TIME)
 split_chord_f4(sleep_time: SLEEP_TIME)
@@ -47,7 +13,8 @@ split_chord_d4(sleep_time: SLEEP_TIME)
 split_chord_g4(sleep_time: SLEEP_TIME)
 split_chord_e4(sleep_time: SLEEP_TIME)
 split_chord_a4(sleep_time: SLEEP_TIME, chord_name: :minor)
-fast_f_fast_g
+split_chord_f4(sleep_time: SLEEP_TIME, split_reps: 1)
+split_chord_g4(sleep_time: SLEEP_TIME, split_reps: 1)
 split_chord_c4(sleep_time: SLEEP_TIME)
 
 split_chord_a4(sleep_time: SLEEP_TIME, chord_name: :minor)
@@ -56,5 +23,6 @@ split_chord_d4(sleep_time: SLEEP_TIME)
 split_chord_g4(sleep_time: SLEEP_TIME)
 split_chord_c4(sleep_time: SLEEP_TIME)
 split_chord_g4(sleep_time: SLEEP_TIME)
-fast_f_fast_g
+split_chord_f4(sleep_time: SLEEP_TIME, split_reps: 1)
+split_chord_g4(sleep_time: SLEEP_TIME, split_reps: 1)
 split_chord_c4(sleep_time: SLEEP_TIME)
