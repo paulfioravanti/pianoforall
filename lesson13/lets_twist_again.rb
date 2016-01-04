604b3c2459ad68393c698ceeaf5440886064c177
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-05 09:00:01 +1100

    Add and refactor twist rhythm

---
+++ lesson13/lets_twist_again.rb
@@ -0,0 +1,19 @@
+# Lesson 13 - Rhythm 6 - Twist
+# Let's Twist Again
+
+require "#{Dir.home}/ruby/pianoforall/lesson13/twist"
+
+use_synth :piano
+
+2.times do
+  twist(:C4)
+end
+2.times do
+  twist(:A3, chord_type: :minor)
+end
+2.times do
+  twist(:F3)
+end
+2.times do
+  twist(:G3)
+end
---
+++ lesson13/twist.rb
@@ -0,0 +1,16 @@
+# Lesson 13 - Rhythm 6 - Twist
+def twist(note = :C4, chord_type: :major, sleep: 0.25)
+  *pitch, octave = note.to_s.chars
+  bass_chord = chord("#{pitch.join}#{octave.to_i - 1}", chord_type)
+
+  3.times do |n|
+    play bass_chord[n]
+    play chord(note, chord_type)
+    sleep sleep
+
+    2.times do
+      play chord(note, chord_type)
+      sleep sleep
+    end
+  end
+end
