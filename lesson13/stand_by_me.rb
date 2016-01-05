ce044611722bfc93a5f2852b65b77dff3b78fa57
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-06 08:27:52 +1100

    Add Stand By Me

---
+++ lesson13/stand_by_me.rb
@@ -0,0 +1,18 @@
+# Lesson 13 - Rhythm 6 - Twist
+# Stand By Me - Ben E King
+
+require "#{Dir.home}/ruby/pianoforall/lesson13/twist"
+
+use_synth :piano
+
+2.times do
+  twist(:C4)
+end
+2.times do
+  twist(:A3, chord_type: :minor)
+end
+twist(:F3)
+twist(:G3)
+2.times do
+  twist(:C4)
+end
