46bd621c8cbcd6812c6150cfd878a30734f7de2e
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-07 08:11:02 +1100

    Add Whiter Shade of Pale

---
+++ lesson14/whiter_shade_of_pale.rb
@@ -0,0 +1,18 @@
+# Lesson 14 - 'Slash' chords and Bass runs
+# Whiter Shade of Pale - Procul Haram
+require "#{Dir.home}/ruby/pianoforall/lesson14/slash_split_chord"
+use_synth :piano
+
+slash_split_chord(:C4)
+slash_split_chord(:C4, bass_note: :B2)
+slash_split_chord(:C4, bass_note: :A2)
+slash_split_chord(:C4, bass_note: :G2)
+slash_split_chord(:F4)
+slash_split_chord(:F4, bass_note: :E3)
+slash_split_chord(:F4, bass_note: :D3)
+slash_split_chord(:F4, bass_note: :C3)
+slash_split_chord(:G4)
+slash_split_chord(:G4, bass_note: :F3)
+slash_split_chord(:G4, bass_note: :E3)
+slash_split_chord(:G4, bass_note: :D3)
+
