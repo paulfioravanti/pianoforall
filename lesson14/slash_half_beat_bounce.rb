29d0b85214035533660a77b002b4a0eff8cc5e1f
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-07 08:55:19 +1100

    Enable multiple bass octaves to be played at the same time

--- lesson14/slash_half_beat_bounce.rb
+++ lesson14/slash_half_beat_bounce.rb
@@ -1,16 +1,22 @@
 # Lesson 14 - 'Slash' chords and Bass runs
 # Half Beat Bounce Rhythm
 
-def slash_half_beat_bounce(note, bass_note: note, quality: :major, beat: 0.5)
+def slash_half_beat_bounce(note, quality: :major, bass_note: note, num_bass_octaves: 1, beat: 0.5)
+  half_beat = beat / 2
   if note == bass_note
     *pitch, octave = note.to_s.chars
     bass_note = "#{pitch.join}#{octave.to_i - 1}"
   end
-  half_beat = beat / 2
+  if num_bass_octaves > 1
+    *pitch, octave = bass_note.to_s.chars
+    bass_notes = (1...num_bass_octaves).reduce([bass_note]) do |memo, num|
+      memo << "#{pitch.join}#{octave.to_i - num}"
+    end
+  end
 
   # note/bass_note
   play chord(note, quality)
-  play bass_note
+  play bass_notes || bass_note
   sleep beat
 
   play chord(note, quality)
