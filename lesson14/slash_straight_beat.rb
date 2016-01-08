abe8fbeb3af960af7f9abc8c77ffe37342dcc6ba
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-08 22:31:23 +1100

    revert making quality a kwarg since it's a non-kwarg in the chord API

--- lesson14/slash_straight_beat.rb
+++ lesson14/slash_straight_beat.rb
@@ -1,6 +1,6 @@
 # Lesson 14 - 'Slash' chords and Bass runs
 # Straight Beat Rhythm/Half Beat Bounce
-def slash_straight_half_beat_bounce(note, quality: :major, bass_note: note, beat: 0.5, reps: 1)
+def slash_straight_half_beat_bounce(note, quality = :major, bass_note: note, beat: 0.5, reps: 1)
   if note == bass_note
     *pitch, octave = note.to_s.chars
     bass_note = "#{pitch.join}#{octave.to_i - 1}"
--- lesson14/sunny_afternoon.rb
+++ lesson14/sunny_afternoon.rb
@@ -3,16 +3,16 @@
 require "#{Dir.home}/ruby/pianoforall/lesson14/slash_straight_beat"
 use_synth :piano
 
-slash_straight_half_beat_bounce(:D4, quality: :minor)
-slash_straight_half_beat_bounce(:D4, quality: :minor, bass_note: :C3)
-slash_straight_half_beat_bounce(:D4, quality: :minor, bass_note: :B2)
-slash_straight_half_beat_bounce(:D4, quality: :minor, bass_note: :Bb2)
+slash_straight_half_beat_bounce(:D4, :minor)
+slash_straight_half_beat_bounce(:D4, :minor, bass_note: :C3)
+slash_straight_half_beat_bounce(:D4, :minor, bass_note: :B2)
+slash_straight_half_beat_bounce(:D4, :minor, bass_note: :Bb2)
 slash_straight_half_beat_bounce(:A3)
 slash_straight_half_beat_bounce(:A3, bass_note: :G2)
 slash_straight_half_beat_bounce(:A3, bass_note: :F2)
 slash_straight_half_beat_bounce(:A3, bass_note: :E2)
 
-slash_straight_half_beat_bounce(:D4, quality: :minor, reps: 2)
+slash_straight_half_beat_bounce(:D4, :minor, reps: 2)
 slash_straight_half_beat_bounce(:C4, reps: 2)
 slash_straight_half_beat_bounce(:F3, reps: 2)
 slash_straight_half_beat_bounce(:C4, reps: 2)
