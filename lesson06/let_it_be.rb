9ce29092cb3bec5f64ddce268d5fdccf4894b241
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-10 15:41:31 +1100

    Invert the bass and treble notations to match how it would be written in slash notation: treble/bass

--- lesson06/let_it_be.rb
+++ lesson06/let_it_be.rb
@@ -1,43 +1,59 @@
 # Lesson 6 - Gospel Song Chord Progression
-# Let It Be
+# Let It Be - The Beatles
 use_synth :piano
+use_bpm 45 # defaults to 60
 
+play chord(:C4)
 play :C3
+sleep 0.5
+
 play chord(:C4)
 sleep 0.5
-play chord(:C4)
-sleep 0.5
+
+play chord(:G4)
 play :G3
+sleep 0.5
+
 play chord(:G4)
 sleep 0.5
-play chord(:G4)
-sleep 0.5
+
+play chord(:A4, :minor)
 play :A3
+sleep 0.5
+
 play chord(:A4, :minor)
 sleep 0.5
-play chord(:A4, :minor)
-sleep 0.5
+
+play chord(:F4)
 play :F3
+sleep 0.5
+
 play chord(:F4)
 sleep 0.5
-play chord(:F4)
-sleep 0.5
+
+play chord(:C4)
 play :C3
+sleep 0.5
+
 play chord(:C4)
 sleep 0.5
-play chord(:C4)
-sleep 0.5
+
+play chord(:G4)
 play :G3
+sleep 0.5
+
 play chord(:G4)
 sleep 0.5
-play chord(:G4)
-sleep 0.5
-play :F3
+
 play chord(:F4)
+play :F3
 sleep 0.5
+
 play chord(:E4, :minor)
 sleep 0.25
+
 play chord(:D4, :minor)
 sleep 0.25
-play :C3
+
 play chord(:C4)
+play :C3
