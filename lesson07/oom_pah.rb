c8013ce2093f0b98c51da5217cabff4225a3baa5
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-10 16:52:18 +1100

    Generalise out the oom path algorithm

--- lesson07/oom_pah.rb
+++ lesson07/oom_pah.rb
@@ -1,18 +1,23 @@
 # Lesson 7 - Rhythm 3 - 'Oom Pah'
-use_synth :piano
+def oom_pah(note = :C4, quality = :major, delay: 0.5)
+  *pitch, octave = note.to_s.chars
+  oom = "#{pitch.join}#{octave.to_i - 1}"
+  # The `chord` method returns a ring of MIDI integers (not note symbols).
+  # In order to get the fifth note of the chord one octave down, we take the
+  # integer and subtract 12 (one octave contains 12 MIDI keys)
+  pah = chord(oom, quality).last - 12
 
-4.times do
-  play :C3
-  play chord(:C4)
-  sleep 0.5
+  play chord(note, quality)
+  play oom
+  sleep delay
 
-  play chord(:C4)
-  sleep 0.5
+  play chord(note, quality)
+  sleep delay
 
-  play :G2
-  play chord(:C4)
-  sleep 0.5
+  play chord(note, quality)
+  play pah
+  sleep delay
 
-  play chord(:C4)
-  sleep 0.5
+  play chord(note, quality)
+  sleep delay
 end
