fdefdec35bed95d134173c6e1a36b83191c37b8f
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-17 14:18:48 +1030

    Add algorithm for 3 beat bounce

--- lesson15/three_beat_bounce.rb
+++ lesson15/three_beat_bounce.rb
@@ -1,7 +1,5 @@
-# Lesson 14 - 'Slash' chords and Bass runs
-# Half Beat Bounce Rhythm
-
-def slash_half_beat_bounce(note, quality: :major, bass_note: note, num_bass_octaves: 1, delay: 0.5)
+# Lesson 15 - Rhythm 7 - Three Beat Bounce
+def three_beat_bounce(note = :C4, quality = :major, bass_note: note, num_bass_octaves: 1, delay: 0.5)
   half_delay = delay / 2
   if note == bass_note
     *pitch, octave = note.to_s.chars
@@ -9,15 +7,22 @@ def slash_half_beat_bounce(note, quality: :major, bass_note: note, num_bass_octa
   end
   if num_bass_octaves > 1
     *pitch, octave = bass_note.to_s.chars
-    bass_notes = (1...num_bass_octaves).reduce([bass_note]) do |memo, num|
-      memo << "#{pitch.join}#{octave.to_i - num}"
-    end
+    bass_notes =
+      (1...num_bass_octaves).reduce([bass_note]) do |memo, num|
+        memo << "#{pitch.join}#{octave.to_i - num}"
+      end
   end
 
   # note / bass_note
   play chord(note, quality)
   play bass_notes || bass_note
   sleep delay
+
+  play chord(note, quality)
+  sleep half_delay
+
+  play bass_note
+  sleep half_delay
 
   play chord(note, quality)
   sleep half_delay
