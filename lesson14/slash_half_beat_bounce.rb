1eb0b6b5599d407f6a1e33009d0a32cc38b90386
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-17 14:18:00 +1030

    Minor syntax change

--- lesson14/slash_half_beat_bounce.rb
+++ lesson14/slash_half_beat_bounce.rb
@@ -1,8 +1,8 @@
 # Lesson 14 - 'Slash' chords and Bass runs
 # Half Beat Bounce Rhythm
 
-def slash_half_beat_bounce(note, quality: :major, bass_note: note, num_bass_octaves: 1, beat: 0.5)
-  half_beat = beat / 2
+def slash_half_beat_bounce(note, quality: :major, bass_note: note, num_bass_octaves: 1, delay: 0.5)
+  half_delay = delay / 2
   if note == bass_note
     *pitch, octave = note.to_s.chars
     bass_note = "#{pitch.join}#{octave.to_i - 1}"
@@ -17,11 +17,11 @@ def slash_half_beat_bounce(note, quality: :major, bass_note: note, num_bass_octa
   # note/bass_note
   play chord(note, quality)
   play bass_notes || bass_note
-  sleep beat
+  sleep delay
 
   play chord(note, quality)
-  sleep half_beat
+  sleep half_delay
 
   play bass_note
-  sleep half_beat
+  sleep half_delay
 end
