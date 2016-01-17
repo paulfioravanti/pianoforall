8a020b77cae7af953da797ab8e0351762d76faa9
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-01-17 16:45:07 +1030

    Refactor country shuffle

--- lesson16/12_8_country_shuffle.rb
+++ lesson16/12_8_country_shuffle.rb
@@ -1,21 +1,8 @@
 # Lesson 16 - Rhythm 8 - 12/8 Country Shuffle
-def country_shuffle(note = :C4, quality = :major, bass_note: note, delay: 0.25, reps: 1)
-  two_beat_delay = delay * 2
-  four_beat_delay = delay * 4
-  if note == bass_note
-    *pitch, octave = note.to_s.chars
-    bass_root = "#{pitch.join}#{octave.to_i - 1}"
-    bass_5 = chord(bass_root, quality).last - 12
-  else
-    bass_root = bass_note
-    bass_5 = chord(bass_root, quality).last - 12
-  end
-  if note.respond_to?(:to_a)
-    treble_root, treble_3_5 = note.shift, note
-  else
-    treble_root = note
-    treble_3_5 = chord(note, quality).to_a.last(2)
-  end
+def country_shuffle(note = :C4, quality = :major, bass_note: note, delay: 0.25)
+  two_beat_delay, four_beat_delay = delay * 2, delay * 4
+  bass_root, bass_5 = extract_bass_root_and_5(note, quality, bass_note)
+  treble_root, treble_3_5 = extract_treble_split_chord(note, quality)
 
   [bass_root, bass_5].each do |bass|
     play bass, sustain: four_beat_delay
@@ -30,4 +17,27 @@ def country_shuffle(note = :C4, quality = :major, bass_note: note, delay: 0.25, 
     play treble_root
     sleep delay
   end
+end
+
+def extract_bass_root_and_5(note, quality, bass_note)
+  if note == bass_note
+    *pitch, octave = note.to_s.chars
+    bass_root = "#{pitch.join}#{octave.to_i - 1}"
+    bass_5 = chord(bass_root, quality).last - 12
+  else
+    bass_root = bass_note
+    bass_5 = chord(bass_root, quality).last - 12
+  end
+  [bass_root, bass_5]
+end
+
+def extract_treble_split_chord(note, quality)
+  if note.respond_to?(:to_a)
+    treble_root = note.shift
+    treble_3_5 = note
+  else
+    treble_root = note
+    treble_3_5 = chord(note, quality).to_a.last(2)
+  end
+  [treble_root, treble_3_5]
 end
---
+++ lesson16/12_8_country_shuffle_practice.rb
@@ -0,0 +1,6 @@
+# Lesson 16 - Rhythm 8 - 12/8 Country Shuffle
+require "#{Dir.home}/ruby/pianoforall/lesson16/12_8_country_shuffle"
+use_synth :piano
+
+2.times { country_shuffle(:C4) }
+2.times { country_shuffle(:F4) }
