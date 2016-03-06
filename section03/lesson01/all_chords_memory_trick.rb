277b705ad353738e43cae6a0deb975900e6748bf
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-03-06 21:51:19 +1100

    Start Section 3

---
+++ section03/lesson01/all_chords_memory_trick.rb
@@ -0,0 +1,76 @@
+# Section 2, Lesson 1 - Memory Trick for all Chords
+require "#{Dir.home}/ruby/pianoforall/utilities"
+use_synth :piano
+
+# All 24 'basic' Major and Minor Chords
+play chord(:C4)
+sleep MINIM
+
+play chord(:C4, :minor)
+sleep MINIM
+
+play chord(:Db4)
+sleep MINIM
+
+play chord(:Db4, :minor)
+sleep MINIM
+
+play chord(:D4)
+sleep MINIM
+
+play chord(:D4, :minor)
+sleep MINIM
+
+play chord(:Eb4)
+sleep MINIM
+
+play chord(:Eb4, :minor)
+sleep MINIM
+
+play chord(:E4)
+sleep MINIM
+
+play chord(:E4, :minor)
+sleep MINIM
+
+play chord(:F4)
+sleep MINIM
+
+play chord(:F4, :minor)
+sleep MINIM
+
+play chord(:Gb4)
+sleep MINIM
+
+play chord(:Gb4, :minor)
+sleep MINIM
+
+play chord(:G4)
+sleep MINIM
+
+play chord(:G4, :minor)
+sleep MINIM
+
+play chord(:Ab4)
+sleep MINIM
+
+play chord(:Ab4, :minor)
+sleep MINIM
+
+play chord(:A4)
+sleep MINIM
+
+play chord(:A4, :minor)
+sleep MINIM
+
+play chord(:Bb4)
+sleep MINIM
+
+play chord(:Bb4, :minor)
+sleep MINIM
+
+play chord(:B4)
+sleep MINIM
+
+play chord(:B4, :minor)
+sleep MINIM
