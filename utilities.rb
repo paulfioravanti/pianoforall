359a6fe23d7d49b85ec6ca293cab6294180f747b
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-03-13 16:41:49 +1100

    Add in constant for semiquaver

--- utilities.rb
+++ utilities.rb
@@ -5,3 +5,4 @@ SEMIBREVE = BREVE / 2.0 # Whole note => 1.0
 MINIM = SEMIBREVE / 2.0 # Half note => 0.5
 CROTCHET = MINIM / 2.0 # Quarter note => 0.25
 QUAVER = CROTCHET / 2.0 # Eighth note => 0.125
+SEMIQUAVER = QUAVER / 2.0 # Sixteenth note => 0.0625
