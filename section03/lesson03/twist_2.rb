d38cdb1b6ed6f8dca6fb149ed929597194ee0457
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-03-13 17:25:39 +1100

    Get glissando timing right

--- section03/lesson03/twist_2.rb
+++ section03/lesson03/twist_2.rb
@@ -4,16 +4,15 @@ require "#{Dir.home}/ruby/pianoforall/utilities"
 
 def twist_2_treble(note = :C4, quality = :major, shift: 0, glissando: false)
   treble_chord = invert_chord(chord(note, quality), shift)
-  one_third_delay = CROTCHET * 0.33
-  three_quarter_delay = CROTCHET * 0.75
+
   sleep CROTCHET
 
   if glissando
     3.times do |n|
       play treble_chord[n]
-      sleep one_third_delay
+      sleep SEMIQUAVER
     end
-    sleep MINIM + three_quarter_delay
+    sleep CROTCHET + MINIM
   else
     play treble_chord, sustain: MINIM
     sleep SEMIBREVE
@@ -28,12 +27,12 @@ end
 
 def twist_2_bass(note = :C3, quality = :major, shift: 0, glissando: false)
   bass_chord = invert_chord(chord(note, quality), shift)
-  three_quarter_delay = CROTCHET * 0.75
+
   play bass_chord[0]
   sleep CROTCHET
 
   if glissando
-    sleep CROTCHET + three_quarter_delay
+    sleep (SEMIQUAVER * 3) + CROTCHET
   else
     sleep MINIM
   end
@@ -43,16 +42,4 @@ def twist_2_bass(note = :C3, quality = :major, shift: 0, glissando: false)
 
   play bass_chord[2]
   sleep MINIM
-end
-
-use_synth :piano
-
-in_thread(name: :right_hand) do
-  twist_2_treble(glissando: true)
-  twist_2_treble
-end
-
-in_thread(name: :left_hand) do
-  twist_2_bass(glissando: true)
-  twist_2_bass
 end
