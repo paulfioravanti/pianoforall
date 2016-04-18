41b463ec55322e30fc973c9e5857a8c04449e63b
Paul Fioravanti <paul.fioravanti@gmail.com>
2016-04-18 16:37:21 +1000

    Prepare rhythms for lesson 5

---
+++ section03/lesson05/three_beat_bounce.rb
@@ -0,0 +1,33 @@
+# Section 3 - Lesson 3 - New Chords Cm, Fm, Gm, Db, Eb, Ab
+# Three Beat Bounce
+require "#{Dir.home}/ruby/pianoforall/utilities"
+
+def three_beat_bounce_treble(note = :C4, quality = :major, shift: 0)
+  treble_chord = invert_chord(chord(note, quality), shift)
+
+  play treble_chord
+  sleep MINIM
+
+  2.times do
+    play treble_chord
+    sleep MINIM
+  end
+end
+
+def three_beat_bounce_bass(note = :C3, num_octaves: 1)
+  if num_octaves > 1
+    *pitch, octave = bass_note.to_s.chars
+    bass_notes = (1...num_bass_octaves).reduce([note]) do |notes, num|
+      notes << "#{pitch.join}#{octave.to_i - num}"
+    end
+  end
+
+  play (bass_notes || note), amp: 0.5
+  sleep MINIM
+
+  2.times do
+    sleep CROTCHET
+    play note, amp: 0.5
+    sleep CROTCHET
+  end
+end
